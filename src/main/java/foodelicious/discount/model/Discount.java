5d1a560106677d2cb4952728786b3a0190a750fb
ricky <krisding7774@gmail.com>
2021-12-23 22:14:24 +0800

    柏翔-12/23 本地可以正常使用的版本 其他人測試有問題再修改

--- src/main/java/foodelicious/discount/model/Discount.java
+++ src/main/java/foodelicious/discount/model/Discount.java
@@ -9,15 +9,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 @Entity
 @Table(name = "discount")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Discount implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -32,5 +25,29 @@ public class Discount implements Serializable {
 
 	@Column(name = "discount_content")
 	private String discount_content;
+
+	public Long getDiscount_id() {
+		return discount_id;
+	}
+
+	public void setDiscount_id(Long discount_id) {
+		this.discount_id = discount_id;
+	}
+
+	public String getDiscount_name() {
+		return discount_name;
+	}
+
+	public void setDiscount_name(String discount_name) {
+		this.discount_name = discount_name;
+	}
+
+	public String getDiscount_content() {
+		return discount_content;
+	}
+
+	public void setDiscount_content(String discount_content) {
+		this.discount_content = discount_content;
+	}
 
 }
