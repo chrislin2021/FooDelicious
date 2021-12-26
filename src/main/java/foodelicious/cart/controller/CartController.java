f71a72c670babea86364e6c48d770570757be8ad
ricky <krisding7774@gmail.com>
2021-12-26 19:46:39 +0800

    柏翔-一個method

--- src/main/java/foodelicious/cart/controller/CartController.java
+++ src/main/java/foodelicious/cart/controller/CartController.java
@@ -1,16 +1,36 @@
 package foodelicious.cart.controller;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.SessionAttributes;
 
+import foodelicious.cart.model.Cart;
 import foodelicious.cart.model.CartService;
+import foodelicious.member.model.Member;
 
 @RestController
-@SessionAttributes("cart")
+@RequestMapping("/shoppingCart")
 public class CartController {
 
 	@Resource
 	private CartService cartService;
+
+	@GetMapping("/index")
+	public String shoppingCart() {
+		return "shoppingCart";
+	}
+
+	@GetMapping("/{id}")
+	public String shoppingCart(@PathVariable(name = "id") Member member_id, Model model) {
+		List<Cart> cartList = cartService.findCartListByMember(member_id);
+		model.addAttribute(cartList);
+		return "/shoppingCart";
+	}
+
 }
--- src/main/java/foodelicious/cart/model/Cart.java
+++ src/main/java/foodelicious/cart/model/Cart.java
@@ -34,13 +34,16 @@ public class Cart implements Serializable {
 
 	@ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
 	@JoinColumn(name = "product_id")
-	private Product product;
+	private Product product_id;
 
 	@Column(name = "cart_quantity")
-	private Integer cart_quantity;
+	private Integer product_quantity;
 
 	@Column(name = "cart_unitPrice")
-	private Integer cart_unitPrice;
+	private Integer product_unitPrice;
+
+	@Column(name = "cart_totalPrice")
+	private Integer cart_totalPrice;
 
 	public Long getCart_id() {
 		return cart_id;
@@ -58,28 +61,36 @@ public class Cart implements Serializable {
 		this.member = member;
 	}
 
-	public Product getProduct() {
-		return product;
+	public Product getProduct_id() {
+		return product_id;
 	}
 
-	public void setProduct_id(Product product) {
-		this.product = product;
+	public void setProduct_id(Product product_id) {
+		this.product_id = product_id;
 	}
 
-	public Integer getCart_quantity() {
-		return cart_quantity;
+	public Integer getProduct_quantity() {
+		return product_quantity;
 	}
 
-	public void setCart_quantity(Integer cart_quantity) {
-		this.cart_quantity = cart_quantity;
+	public void setProduct_quantity(Integer product_quantity) {
+		this.product_quantity = product_quantity;
 	}
 
-	public Integer getCart_unitPrice() {
-		return cart_unitPrice;
+	public Integer getProduct_unitPrice() {
+		return product_unitPrice;
 	}
 
-	public void setCart_unitPrice(Integer cart_unitPrice) {
-		this.cart_unitPrice = cart_unitPrice;
+	public void setProduct_unitPrice(Integer product_unitPrice) {
+		this.product_unitPrice = product_unitPrice;
+	}
+
+	public Integer getCart_totalPrice() {
+		return cart_totalPrice;
+	}
+
+	public void setCart_totalPrice(Integer cart_totalPrice) {
+		this.cart_totalPrice = cart_totalPrice;
 	}
 
 }
