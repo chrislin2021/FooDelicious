87b91ec0754c81c91aa7dfdabf25dae713321149
Felix <xc1zt1x2005@gmail.com>
2021-12-27 08:24:02 +0800

    漢彬-錯誤修正 自動建立表格別再開啟

--- src/main/java/foodelicious/article/controller/ArticleController.java
+++ src/main/java/foodelicious/article/controller/ArticleController.java
@@ -1,6 +1,9 @@
 package foodelicious.article.controller;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,11 +12,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class ArticleController {
 
 	@ResponseBody
-	@RequestMapping(path = "/imgArticle", consumes = "multipart/form-data", method = RequestMethod.POST)
-	public String imgArticle() {
-
-//		System.out.println(params);
+	@RequestMapping(path = "/imgArticle", consumes = "multipart/form-data",method = RequestMethod.POST)
+	public String imgArticle(@RequestBody() Map<String, String> test) {
 		System.out.println("輸出測試");
-		return "輸出測試";
+		System.out.println(test);
+
+		return "111";
 	}
 }
