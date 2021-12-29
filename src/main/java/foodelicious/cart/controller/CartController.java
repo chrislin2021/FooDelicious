dc8c3bbd3b049a1f7c7568765d289fe02b545834
ricky <krisding7774@gmail.com>
2021-12-29 14:53:10 +0800

    柏翔-12/29 controller 新增查詢、刪除

--- src/main/java/foodelicious/cart/controller/CartController.java
+++ src/main/java/foodelicious/cart/controller/CartController.java
@@ -2,7 +2,6 @@ package foodelicious.cart.controller;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import javax.annotation.Resource;
 
@@ -11,15 +10,10 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import foodelicious.cart.model.CartBean;
 import foodelicious.cart.model.CartService;
-import foodelicious.member.model.Member;
-import foodelicious.product.model.Product;
-import foodelicious.product.model.ProductService;
-import freemarker.template.utility.CollectionUtils;
 
 @RestController
 public class CartController {
@@ -27,43 +21,28 @@ public class CartController {
 	@Resource
 	private CartService cartService;
 
-	@Resource
-	private ProductService productService;
-
 	@GetMapping("/shoppingCart")
 	public String shoppingCart() {
 		return "shoppingCart";
 	}
 
-//	@GetMapping("/shoppingCart/{id}")
-//	public String showCart(@PathVariable Long id, Model model) {
-//		System.out.println(id + "[==========================================");
-//		Member member = new Member();
-//		member.setMember_id(id);
-//		ArrayList<CartBean> carts = (ArrayList<CartBean>) cartService.findByMember(member);
-//		model.addAttribute("carts", carts);
-//		for (CartBean cart : carts) {
-//			System.out.println(cart.getCart_id());
-//			System.out.println(cart.getMember().getUserName());
-//			System.out.println(cart.getProduct().getProduct_name());
-//			System.out.println(cart.getQuantity());
-//			System.out.println(cart.getUnitPrice());
-//			System.out.println(cart.getTotalPrice());
-//		}
-//		return "app.ShoppingCart";
-//	}
+	@GetMapping("/shoppingCart/{id}")
+	public List<CartBean> showCart(@PathVariable Long id, Model m) {
+		ArrayList<CartBean> carts = (ArrayList<CartBean>) cartService.selectItem(id);
+		m.addAttribute("carts", carts);
+		return carts;
+	}
 
-//	@PostMapping("/shoppingCart/add")
-//	public String addToCart(Model m, Cart cart) {
-//		Product product = productService
-//		return null;
-//
-//	}
+	@PostMapping("/shoppingCart/add")
+	public String addToCart(Model m, CartBean cart) {
 
-//	@DeleteMapping("/shoppingCart/{id}")
-//	public String shoppingCart(@PathVariable Integer id, Model m) {
-//		cartService.deleteById(id);
-//		return "redirect:/cart/show";
-//	}
+		return null;
+	}
+
+	@DeleteMapping("/delete/{id}")
+	public String shoppingCart(@PathVariable Long id, Model m) {
+		cartService.deleteItem(id);
+		return "redirect:/cart/show";
+	}
 
 }
--- src/main/java/foodelicious/cart/model/CartRepository.java
+++ src/main/java/foodelicious/cart/model/CartRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CartRepository extends JpaRepository<CartBean, Long> {
 
-//	JPA自定義方法不能有底線都把底線去除
+//	JPA自定義方法不能有底線把底線去除
 	List<CartBean> findAllByMemberId(Long memberId);
 
 }
