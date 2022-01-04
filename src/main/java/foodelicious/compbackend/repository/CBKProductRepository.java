e7b30a9239f1977d6236fb9b39ac8af2f7b45235
Drew <jinglu.sw@gmail.com>
2022-01-04 15:30:33 +0800

    Drew- 重推

--- src/main/java/foodelicious/compbackend/repository/CBKProductRepository.java
+++ src/main/java/foodelicious/compbackend/repository/CBKProductRepository.java
@@ -1,13 +1,15 @@
 package foodelicious.compbackend.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import foodelicious.product.model.Product;
 
 public interface CBKProductRepository extends CrudRepository<Product, Integer>{
 	
-	@Query(value="SELECT * FROM member_data2 WHERE member_mail LIKE %?%", nativeQuery = true)
-	List<Product> products
+	
 	
 	
 
