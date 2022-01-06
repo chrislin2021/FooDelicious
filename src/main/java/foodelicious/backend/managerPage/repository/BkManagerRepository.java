8002314571c9bbec29832029ae54b6ed64ebdfc3
Felix <xc1zt1x2005@gmail.com>
2022-01-06 21:21:58 +0800

    漢彬-資料夾名稱修改

--- src/main/java/foodelicious/backend/managerPage/repository/BkManagerRepository.java
+++ src/main/java/foodelicious/backend/managerPage/repository/BkManagerRepository.java
@@ -1,4 +1,4 @@
-package foodelicious.backend.mamagerPage.repository;
+package foodelicious.backend.managerPage.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
